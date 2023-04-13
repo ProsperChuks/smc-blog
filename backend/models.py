1d8432ec7ccfda49b1ae33ff1395cd6f088ffad5
Prosper Chuks <prosperc40@gmail.com>
2023-04-13 13:19:38 +0100

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -62,7 +62,7 @@ class post(models.Model):
     body = RichTextField()
     mainImage = models.ImageField(upload_to='post')
     video = models.FileField(upload_to='post/vd_uploads', blank=True, null=True)
-    image_slide = models.ManyToManyField('imageShow')
+    image_slide = models.ManyToManyField('imageShow', blank=True)
 
     class Meta:
         ordering = ('-publishedAt',)
