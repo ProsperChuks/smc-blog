d21dc47cb1a9a4cf47ac8c515dc6f9e49707ee94
FemiBlack <tadefemi2@hotmail.com>
2023-04-17 14:54:33 +0100

    enable debug mode(temp)

--- blog_smc/settings.py
+++ blog_smc/settings.py
@@ -33,8 +33,8 @@ if 'SECRET_KEY' in os.environ:
     SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if not IS_HEROKU:
-    DEBUG = True
+# if not IS_HEROKU:
+DEBUG = True
 
 if IS_HEROKU:
     ALLOWED_HOSTS = ["*"]
